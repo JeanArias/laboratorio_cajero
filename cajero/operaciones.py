4a90a576ff20fa60060a54add74b02bbe6b182af
Eduardo S <esolanoa@ucenfotec.ac.cr>
2026-07-20 20:33:40 -0600

    Eduardo
    Codigo actualizado

--- cajero/operaciones.py
+++ cajero/operaciones.py
@@ -1,6 +1,16 @@
+class FondosInsuficientesError(Exception):
+    """Excepción para cuando el monto a retirar supera el saldo disponible."""
+    pass
 
+class MontoMinimoError(Exception):
+    """Excepción para cuando el monto es menor al mínimo permitido ($10)."""
+    pass
 
 def retirar_dinero(saldo, monto):
-
-
+    if monto < 10:
+        raise MontoMinimoError("El monto mínimo de retiro es de $10.")
+    
+    if monto > saldo:
+        raise FondosInsuficientesError("No tienes fondos suficientes para realizar este retiro.")
+    
     return saldo - monto
